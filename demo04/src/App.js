29db466dff5e7fed4c632961abde0231b81a6ec5
lodorive <s_s0309@naver.com>
2023-11-02 15:31:34 +0900

    일정관리앱 수정 구현중

--- demo04/src/App.js
+++ demo04/src/App.js
@@ -12,15 +12,12 @@ function App() {
     { no: 4, title: "친구만나기", type: "일상", edit:false }
 ]);
 
-const deleteTodoList = (todo) =>{
-  const newTodoList = todoList.filter(target => target.no !== todo.no);
-  setTodoList(newTodoList)
-};
 
-const [data, setData] = useState({
-  title:"", 
-  type:""
-});
+//등록을 위한 state
+const [data, setData] = useState({title:"", type:""});
+
+//수정을 위한 state
+const [editData, setEditData] = useState({title:"", type:""});
 
 const changeData = (e)=>{
   setData({
@@ -31,10 +28,10 @@ const changeData = (e)=>{
 
 const addTodoList = ()=>{
   //data의 내용을 todoList에 추가 후 data를 초기화
-
+  
   //내용 검사 코드 추가 if(맘에 안들면) return;
   if(data.title.length === 0 || data.type.length === 0) return;
-
+  
   const last = todoList.length - 1; //마지막 번호
   const no = todoList.length === 0 ? 1 : todoList[last].no + 1;
 
@@ -45,13 +42,32 @@ const addTodoList = ()=>{
       no:no
     } 
   ]);
-
+  
   //초기화
   setData({title:"", type:""});
 };
 
-  return (
-    <div className='container-fluid my-3'>
+//todo 항목 삭제
+const deleteTodoList = (todo) =>{
+  const newTodoList = todoList.filter(target => target.no !== todo.no);
+  setTodoList(newTodoList)
+};
+
+//todo 수정을 위한 선택
+const editTodoList = (todo) => {
+  //setEditData(todo) //안되는 코드(얕은 복사, shallow copy) //열쇠만 주는 느낌..
+  setEditData({...todo}); //가능한 코드(깊은 복사, deep copy) 
+};
+
+const changeEditData = (e)=>{
+  setEditData({
+    ...editData,
+    [e.target.name] : e.target.value
+  });
+};
+
+return (
+  <div className='container-fluid my-3'>
       <div className='row'>
         <div className='col-md-8 offset-md-2'>
 
@@ -80,6 +96,21 @@ const addTodoList = ()=>{
             </div>
           </div>
 
+          {/* 수정 화면 */}
+          <div className="row mt-4">
+            <div className="col-6">
+              <input type="text" name="title" value={editData.title} onChange={changeEditData}/>
+            </div>
+            <div className="col-3">
+              <select name="type" value={editData.type} onChange={changeEditData}>
+              <option>일상</option>
+                <option>약속</option>
+                <option>취미</option>
+                <option>공부</option>
+                </select>
+            </div>
+          </div>
+
           {/* 출력 화면 */}
           <div className='row mt-4'>
             {todoList.map(todo=>(
@@ -88,8 +119,10 @@ const addTodoList = ()=>{
                 {todo.type}
               </span>
               {todo.title}
-
-              <FaRegEdit className="text-warning ms-1"/>
+              
+              {/* 수정버튼 */}
+              <FaRegEdit className="text-warning ms-1" onClick={e=>editTodoList(todo)}/>
+              {/* 삭제버튼 */}
               <FaXmark className="text-danger" onClick={e=>deleteTodoList(todo)}/>
             </div>
             ))}
